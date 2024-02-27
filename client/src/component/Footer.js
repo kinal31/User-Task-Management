af1658c73da9b31d50bd6a69886eb504ecb8aed8
Kinal Kotheeya <128071723+kinal31@users.noreply.github.com>
2024-02-27 15:52:03 +0530

    Update Footer.js

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <footer class="bg-body-tertiary text-center text-lg-start fixed-bottom" >
+            <footer className="bg-body-tertiary text-center text-lg-start fixed-bottom" >
                 
-                <div class="text-center p-2" >
+                <div className="text-center p-2" >
                     © 2024 Kinal Kotheeya All rights reserved
                 </div>
                 
